d6ed513c12e0f663c1ff3ac1fe3c8d42e4dbe67e
Amr Mohamed <47578312+amrmoohamed@users.noreply.github.com>
2021-08-13 00:40:10 +0200

    Update download-kline.py

--- python/download-kline.py
+++ python/download-kline.py
@@ -40,7 +40,7 @@ def download_monthly_klines(symbols, num_symbols, intervals, years, months, star
         for month in months:
           current_date = convert_to_date_object('{}-{}-01'.format(year, month))
           if current_date >= start_date and current_date <= end_date:
-            path = "data/spot/monthly/klines/{}/{}/".format(symbol.upper(), interval)
+            path = "data/{}/{}/".format(interval,symbol.upper())
             file_name = "{}-{}-{}-{}.zip".format(symbol.upper(), interval, year, '{:02d}'.format(month))
             download_file(path, file_name, date_range, folder)
 
@@ -78,7 +78,7 @@ def download_daily_klines(symbols, num_symbols, intervals, dates, start_date, en
       for date in dates:
         current_date = convert_to_date_object(date)
         if current_date >= start_date and current_date <= end_date:
-          path = "data/spot/daily/klines/{}/{}/".format(symbol.upper(), interval)
+          path = "data/{}/{}/".format(interval,symbol.upper())
           file_name = "{}-{}-{}.zip".format(symbol.upper(), interval, date)
           download_file(path, file_name, date_range, folder)
 
